3ac8c20b717bd179e55b98e37a0c916441c3068e
Pavlo <pavlo.onishchenko@gmail.com>
2020-01-11 23:14:26 +0200

    'tests_tmp'

---
+++ tmp/ClassLibraryNETFramework/ProjectName.TestsPack1/CodeNameTests.cs
@@ -0,0 +1,89 @@
+using System;
+using NUnit.Framework;
+
+namespace ProjectName.TestsPack1 {
+    public class CodeNameTests {
+        [TestCase("036000291452", "03600029145")]
+        [TestCase("012300000642", "01230000064")]
+        [TestCase("712345678904", "71234567890")]
+        [TestCase("639382000393", "63938200039")]
+        [Category("Positive test cases")]
+        public void CkeckDigitByNotZeroPath_UPCcode_ReturnsUPCcodeWithCkeckDigit(String exp_upc_code_with_check_digit, String upc_code) {
+            Assert.AreEqual(exp_upc_code_with_check_digit, CodeName.Calculate_check_digit(upc_code));
+        }
+
+        [Test]
+        [Category("Positive test cases")]
+        public void CkeckDigitByZeroPath__UPCcode_ReturnsUPCcodeWithCkeckDigit() {
+            Assert.AreEqual("000000000000", CodeName.Calculate_check_digit("00000000000"));
+        }
+
+        [Test]
+        [Category("Negative test cases")]
+        public void CkeckDigitOnInvalidArgument_UPCcodeIsNull_ThrowsException() {
+            Assert.Throws<ArgumentNullException>(() => CodeName.Calculate_check_digit(null));
+        }
+
+        [Test]
+        [Category("Negative test cases")]
+        public void CkeckDigitOnInvalidArgument_UPCcodeIsEmpty_ThrowsException() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CodeName.Calculate_check_digit(""));
+        }
+
+        [Test]
+        [Category("Negative test cases")]
+        public void CkeckDigitOnInvalidArgument_UPCcodeIsToLong_ThrowsException() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CodeName.Calculate_check_digit("712345678904"));
+        }
+
+        [Test]
+        [Category("Negative test cases")]
+        public void CkeckDigitOnInvalidArgument_UPCcodeIsToShort_ThrowsException() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CodeName.Calculate_check_digit("7234567890"));
+        }
+
+        [Test]
+        [Category("Negative test cases")]
+        public void CkeckDigitOnInvalidArgument_InvalidFormatOfUPCcode_ThrowsException() {
+            Assert.Throws<FormatException>(() => CodeName.Calculate_check_digit("7l234567890"));
+        }
+
+        [Test]
+        [Category("Negative test cases")]
+        public void CkeckDigitByNotZeroPath_NotExpectedUPCcode_ReturnsUPCcodeWithCkeckDigit() {
+            Assert.AreNotEqual("712345678904", CodeName.Calculate_check_digit("10010101000"));
+        }
+
+        [Test]
+        [Category("Negative test cases")]
+        public void CkeckDigitByNotZeroPath_ActualNotExpected_ReturnsUPCcodeWithCkeckDigit() {
+            Assert.AreNotEqual("712345678904", CodeName.Calculate_check_digit("11111111111"));
+        }
+
+        [Test]
+        [Category("Negative test cases")]
+        public void CkeckDigitByZeroPath_NotExpectedUPCcode_ReturnsUPCcodeWithCkeckDigit() {
+            Assert.AreNotEqual("919959191090", CodeName.Calculate_check_digit("00000000000"));
+        }
+
+        [Test]
+        [Category("Negative test cases")]
+        public void CkeckDigitByZeroPath_ActualNotExpected_ReturnsUPCcodeWithCkeckDigit() {
+            Assert.AreNotEqual("111111111117", CodeName.Calculate_check_digit("00000000000"));
+        }
+
+        [TestCase("712345678904", "71234567890")]
+        [TestCase("712345678904", "71234567890")]
+        [Category("Negative test cases")]
+        public void CkeckDigitByNotZeroPath_UPCcodeREPEAT_ReturnsUPCcodeWithCkeckDigit(String exp_upc_code_with_check_digit, String upc_code) {
+            Assert.AreEqual(exp_upc_code_with_check_digit, CodeName.Calculate_check_digit(upc_code));
+        }
+
+        [TestCase("000000000000", "00000000000")]
+        [TestCase("000000000000", "00000000000")]
+        [Category("Negative test cases")]
+        public void CkeckDigitByZeroPath_UPCcodeREPEAT_ReturnsUPCcodeWithCkeckDigit(String exp_upc_code_with_check_digit, String upc_code) {
+            Assert.AreEqual(exp_upc_code_with_check_digit, CodeName.Calculate_check_digit(upc_code));
+        }
+    }
+}
---
+++ tmp/ClassLibraryNETFramework/ProjectName/CodeName.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ProjectName {
+    public class CodeName {
+        public static String Calculate_check_digit(String upc_a_code) {
+            if (upc_a_code == null) {
+                throw new ArgumentNullException("UPC-A code is Null\n");
+            }
+
+            int n = upc_a_code.Length;
+
+            if (n != 11) {
+                throw new ArgumentOutOfRangeException($"Invalid length {n} instead of 11\n");
+            }
+
+            if (!Regex.IsMatch(upc_a_code, @"^\d+$")) {
+                throw new FormatException("Invalid format of UPC-A code\n");
+            }
+
+            int od_sum = 0, ev_sum = 0, check_digit = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (i % 2 == 0) {
+                    od_sum += (int)Char.GetNumericValue(upc_a_code[i]);
+                } else {
+                    ev_sum += (int)Char.GetNumericValue(upc_a_code[i]);
+                }
+            }
+
+            int check_digit_tmp = (od_sum * 3 + ev_sum) % 10;
+
+            if (check_digit_tmp != 0) {
+                check_digit = 10 - check_digit_tmp;
+            }
+
+            return upc_a_code + check_digit.ToString();
+        }
+    }
+}
